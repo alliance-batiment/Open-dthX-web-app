951259f304f9656bf0685206564b0090d6fbb0d5
triazur <guillaume.cassin@triazur.com>
2024-06-24 08:17:02 +0200

    save

--- module-federation/overrides/webpack-config.js
+++ module-federation/overrides/webpack-config.js
@@ -5,6 +5,36 @@ const webpackConfig = require(webpackConfigPath);
 
 const override = (config) => {
   config.plugins.push(new ModuleFederationPlugin(require('../../modulefederation.config.js')));
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      { loader: 'style-loader' },
+      {
+        loader: 'css-loader', options: {
+          sourceMap: true, modules: true,
+          localIdentName: '[local]_[hash:base64:5]'
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+          config: {
+            path: 'postcss.config.js'
+          }
+        }
+      },
+      {
+        loader: 'sass-loader', options: { sourceMap: true }
+      }
+    ]
+  });
+
+  // Add resolve fallback for buffer and timers
+  config.resolve.fallback = {
+    "buffer": require.resolve("buffer/"),
+    "timers": require.resolve("timers-browserify")
+  };
 
   config.output.publicPath = 'auto';
 
@@ -14,3 +44,4 @@ const override = (config) => {
 require.cache[require.resolve(webpackConfigPath)].exports = (env) => override(webpackConfig(env));
 
 module.exports = require(webpackConfigPath);
+
